42f89884a04e5d68d089b11afde5a580c981a49b
root <tjthejuggler@gmail.com>
2022-04-02 19:52:19 +0000

    pics

--- main.py
+++ main.py
@@ -1229,7 +1229,7 @@ rand_mag = 0.05
 
 # %%
 text_prompts = [
-    "a beautiful calm girl, between tidal waves and forest fires, matte painting by greg rutkowski and thomas kinkade, trending on artstation"
+    ["happy family eating at the dinner table:2","robots squirting food to happy family:1"]
 ]
 
 image_prompts = [ #currently disabled
